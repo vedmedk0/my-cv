e77ec75c944fec07b3386cd3798e4aec0c973981
Mikhail Golubtsov <mp.golubtcov@physics.msu.ru>
2024-01-17 21:53:50 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,12 @@ sbt "reload ; clean ; fullOptJS ; packageSite" && \
   cd $DIR && \
   git clone https://github.com/vedmedk0/my-cv.git && \
   cd my-cv && \
+  git config --local user.email mp.golubtcov@physics.msu.ru && \
   git checkout gh-pages && \
   touch dummy && \
   rm -rf ./* && \
   cp $CURRENT/target/site/* . && \
-  git add . && \
+  git add --verbose . && \
   git commit -m "$COMMIT_MSG" && \
   git push -f
 
